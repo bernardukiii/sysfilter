a4b054f05f6a3f48c3f556d14e638a68ff3cec49
MrRari14 <ber.97ferrari@gmail.com>
2023-06-21 11:05:26 -0400

    Importing JSON credentials that and setting the environs

--- prettify_syslogs.py
+++ prettify_syslogs.py
@@ -8,6 +8,11 @@ import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Define file location    
 file_path = '/var/log/syslog'
 user_name = 'mrrari14'
@@ -70,9 +75,45 @@ scopes = [
 'https://www.googleapis.com/auth/drive'
 ]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name("syslogs-filter-key.json", scopes) #access the json key you downloaded earlier 
+
+# Load the credentials from the JSON key file and replace environment variables
+credentials_dict = {
+    "type": "service_account",
+    "project_id": os.environ.get("PROJECT_ID"),
+    "private_key_id": os.environ.get("PROJECT_KEY"),
+    "private_key": os.environ.get("PRIVATE_KEY"),
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id": os.environ.get("CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.environ.get("CERT_URL"),
+    "universe_domain": "googleapis.com"
+}
+
+# Load the credentials from the JSON key file
+with open('syslogs-filter-key.json') as json_file:
+    credentials_dict = json.load(json_file)
+
+# Replace the placeholders in the credentials with environment variables
+credentials_dict["project_id"] = os.environ.get("PROJECT_ID")
+credentials_dict["private_key_id"] = os.environ.get("PROJECT_KEY")
+credentials_dict["private_key"] = os.environ.get("PRIVATE_KEY")
+credentials_dict["client_email"] = os.environ.get("CLIENT_EMAIL")
+credentials_dict["client_id"] = os.environ.get("CLIENT_ID")
+credentials_dict["client_x509_cert_url"] = os.environ.get("CERT_URL")
+
+# Set the environment variables
+os.environ["PROJECT_ID"] = credentials_dict["project_id"]
+os.environ["PROJECT_KEY"] = credentials_dict["private_key_id"]
+os.environ["PRIVATE_KEY"] = credentials_dict["private_key"]
+os.environ["CLIENT_EMAIL"] = credentials_dict["client_email"]
+os.environ["CLIENT_ID"] = credentials_dict["client_id"]
+os.environ["CERT_URL"] = credentials_dict["client_x509_cert_url"]
+
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scopes) #access the JSON key
 file = gspread.authorize(credentials) # authenticate the JSON key with gspread
 sheet = file.open("Ubuntu booting logs") #open sheet
 sheet = sheet.worksheet('Logs') #replace sheet_name with the name that corresponds to yours, e.g, it can be sheet1
-cell_value = sheet.acell('A2')
-print(cell_value.value)
+# cell_value = sheet.acell('A2')
+# print(cell_value.value)
